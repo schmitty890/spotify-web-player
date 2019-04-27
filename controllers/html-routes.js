f917bdcee67da22861788ad9a158edbd437284ac
jschmitt <schmitty890@gmail.com>
2019-04-26 22:51:30 -0400

    init

---
+++ controllers/html-routes.js
@@ -0,0 +1,25 @@
+// var axios = require('axios');
+// const { promisify } = require('util');
+// const crypto = require('crypto');
+// const nodemailer = require('nodemailer');
+// const passport = require('passport');
+// const User = require('../models/User');
+// var db = require("../models");
+
+// const randomBytesAsync = promisify(crypto.randomBytes);
+
+module.exports = function (app) {
+  // Home Page
+  app.get('/', function (req, res) {
+    // assign the handlebar object any data to be read into the template. this separates the data from the markup.
+    var hbsObject = {
+      user: req.user
+    };
+    console.log(hbsObject);
+    // console.log(hbsObject);
+    res.render('index', {
+      hbsObject: hbsObject
+    });
+  });
+
+}
